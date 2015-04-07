86ef413cc585da8beab608a17ff265b05525ad90
Orta Therox <orta.therox@gmail.com>
2015-04-07 09:40:03 -0400

    Added some comments

--- lib/cocoapods_acknowledgements.rb
+++ lib/cocoapods_acknowledgements.rb
@@ -12,21 +12,25 @@ module CocoaPodsAcknowledgements
         
         umbrella_target.user_target_uuids.each do |user_target_uuid|
         
+          # Generate a plist representing all of the podspecs
           metadata = PlistGenerator.generate(umbrella_target, sandbox)
           plist_path = sandbox.root + "#{umbrella_target.cocoapods_target_label}-metadata.plist"
           Xcodeproj.write_plist(metadata, plist_path)
 
+          # Find a root folder in the users Xcode Project called CocoaPods, or make one
           project = Xcodeproj::Project.open(umbrella_target.user_project_path)
           cocoapods_group = project.main_group["CocoaPods"]
           unless cocoapods_group
             cocoapods_group = project.main_group.new_group("CocoaPods", sandbox.root)
           end
-
+          
+          # Add the example plist to the found CocoaPods group
           file_ref = cocoapods_group.files.find { |file| file.real_path == plist_path }
           unless file_ref
             file_ref = cocoapods_group.new_file(plist_path)
           end
 
+          # Ensure that the plist is added to target
           target = project.objects_by_uuid[user_target_uuid]
           unless target.resources_build_phase.files_references.include?(file_ref)
             target.add_resources([file_ref])
