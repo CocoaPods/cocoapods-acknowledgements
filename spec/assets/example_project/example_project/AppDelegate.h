e5bc6aa4a4fa9e21575e624304fd9c92a55331cb
Dmitry Lobanov <gaussblurinc@gmail.com>
2018-02-13 21:30:42 +0300

    spec: example xcode project added.

---
+++ spec/assets/example_project/example_project/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  example_project
+//
+//  Created by Lobanov Dmitry on 13.02.2018.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ spec/assets/example_project/example_project/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  example_project
+//
+//  Created by Lobanov Dmitry on 13.02.2018.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
